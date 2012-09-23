3cb8dbd521feb60ccfdb2f778fe4f9c8cf41a404
Johan Govers <att.johgo@gmail.com>
2012-09-23 07:55:50 +0200

    Added Gustafs GOAP project

---
+++ GOAP/PlanningAction.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GOAP
+{
+    class Goal
+    {
+        public string Name;
+        List<Tuple<string, int>> _targets = new List<Tuple<string, int>>();
+        
+        public Goal(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public Goal Target(string item, int quantity)
+        {
+            _targets.Add(new Tuple<string, int>(item, quantity));
+            return this;
+        }
+
+        public double Fulfillment(State s)
+        {
+            double score = 0.0;
+            foreach (var tgt in _targets)
+            {
+                double actual = s.ItemQuantity(tgt.Item1);
+                if (actual > tgt.Item2) actual = tgt.Item2;
+                score += actual / tgt.Item2;
+            }
+            return score / _targets.Count();
+        }
+    }
+
+    class PlanningAction : ICloneable
+    {
+        public string Name;
+        bool multiProducer = false;
+
+        List<string> _requires=new List<string>();
+        List<Tuple<string, int>> _consumes = new List<Tuple<string, int>>();
+        List<Tuple<string, int>> _produces = new List<Tuple<string, int>>();
+        List<Action<State>> _postActions = new List<Action<State>>();
+
+        public PlanningAction(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public PlanningAction AssignPostAction(Action<State> PostAction)
+        {
+            _postActions.Add(PostAction);
+            return this;
+        }
+
+        public bool CanExecute(State s)
+        {
+            if (_requires.Exists(l => !s.Has(l))) return false;
+            if (_consumes.Exists(l => !s.Sufficient(l.Item1,l.Item2))) return false;
+            return true;
+        }
+
+        public State Migrate(State s)
+        {
+            var newState = s.Clone() as State;
+            Execute(newState);
+            return newState;
+        }
+
+        public void Execute(State s)
+        {
+            _consumes.ForEach(c => s.ReduceItem(c.Item1, c.Item2));
+            if (multiProducer)
+            {
+                int quantity = s.ItemQuantity(_requires.First());
+                _produces.ForEach(p => s.AddItem(p.Item1, p.Item2 * quantity));
+           
+            }
+            else
+            {
+                _produces.ForEach(p => s.AddItem(p.Item1, p.Item2));
+            }
+            _postActions.ForEach(pa => pa(s));
+        }
+
+        public PlanningAction Requires(string item)
+        {
+            _requires.Add(item);
+            return this;
+        }
+        public PlanningAction Consumes(string item,int quantity)
+        {
+            _consumes.Add(new Tuple<string,int>(item,quantity));
+            return this;
+        }
+        public PlanningAction Consumes(string item)
+        {
+            return Consumes(item, 1);
+        }
+        public PlanningAction Produces(string item, int quantity)
+        {
+            _produces.Add(new Tuple<string, int>(item, quantity));
+            return this;
+        }
+        public PlanningAction Produces(string item)
+        {
+            return Produces(item, 1);
+        }
+
+        public PlanningAction MultiProducer()
+        {
+            multiProducer = true;
+            return this;
+        }
+
+
+
+        public object Clone()
+        {
+            var cloned = new PlanningAction(this.Name);
+            cloned.multiProducer = this.multiProducer;
+            foreach (var req in _requires)
+            {
+                cloned._requires.Add(req);
+            }
+            foreach (var c in _consumes)
+            {
+                cloned._consumes.Add(new Tuple<string, int>(c.Item1, c.Item2));
+            }
+            foreach (var p in _produces)
+            {
+                cloned._produces.Add(new Tuple<string, int>(p.Item1, p.Item2));
+            }
+            return cloned;
+        }
+    }
+
+    static class PlanningActionHandler
+    {
+        /*public static IPerishingAction Perishes()
+        {
+            return new PerishingAction();
+        }*/
+
+    }
+
+}
---
+++ GOAP/State.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GOAP
+{
+    class State : ICloneable
+    {
+        public Dictionary<string, int> Items;
+        public List<PlanningAction> PlanningActions;
+        public State()
+        {
+            Items = new Dictionary<string, int>();
+            PlanningActions = new List<PlanningAction>();
+        }
+        public void AddItem(string item)  {
+            AddItem(item, 1);
+        }
+        public void AddItem(string item, int quantity)
+        {
+            if (!Items.ContainsKey(item))
+            {
+                Items.Add(item, quantity);
+            }
+            else
+            {
+                Items[item] += quantity;
+            }
+        }
+        public void ReduceItem(string item, int quantity)
+        {
+            if (Items.ContainsKey(item))
+            {
+                Items[item] -= quantity;
+            }
+        }
+        
+        public void RemoveItem(string item)
+        {
+            Items.Remove(item);
+        }
+        public int ItemQuantity(string item)
+        {
+            if (!Items.ContainsKey(item))
+            {
+                return 0;
+            }
+            return Items[item];
+        }
+        public bool Sufficient(string item, int quantity)
+        {
+            if (!Items.ContainsKey(item))
+            {
+                return false;
+            }
+            if (Items[item] < quantity) return false;
+            return true;
+        }
+        public bool Has(string item)
+        {
+            return Sufficient(item, 1);
+        }
+
+        public object Clone()
+        {
+            var cloned = new State();
+            foreach (var item in Items)
+            {
+                cloned.AddItem(item.Key, item.Value);
+            }
+            foreach (var pa in this.PlanningActions)
+            {
+                cloned.PlanningActions.Add((PlanningAction)pa.Clone());
+            }
+            return cloned;
+        }
+    }
+}
