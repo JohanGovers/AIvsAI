b7e7413ee12827a48b4a650ac152bba35cfcde1a
JohanGov <att.johgo@gmail.com>
2012-10-10 21:22:19 +0200

    Started implementation of Breadth First Search

---
+++ GOAP/Planners/BFSPlan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GOAP.Planners
+{
+    public class BFSPlan : IPlan
+    {
+        private Queue<State> stateProcessQueue = new Queue<State>();
+
+        public void Search(State state, Goal goal)
+        {
+            stateProcessQueue.Enqueue(state);
+
+            InnerSearch(state, goal);
+        }
+
+        private void InnerSearch(State state, Goal goal)
+        {
+            // Evaluate exit criteria
+
+            // Put each state in a queue
+
+            // Recurse
+        }
+
+        public Stack<PlanningAction> GetPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public State GetFinalState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
