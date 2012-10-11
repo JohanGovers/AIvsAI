88166c848950b8eadc4507d2eb5561260ba5e312
JohanGov <att.johgo@gmail.com>
2012-10-11 21:28:20 +0200

    Implementation of Breadth first search... At least some tests pass.

---
+++ GOAP.Test/BFSPlanTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using GOAP.Planners;
+
+namespace GOAP.Test
+{
+    [TestFixture]
+    public class BFSPlanTest
+    {
+        [Test]
+        public void PerformOnlyActionInPlan()
+        {
+            string actionName = "Build Harvester";
+            var state = new State();
+            state.PlanningActions.Add(new PlanningAction(actionName).Produces("Harvester"));
+            var g0 = new Goal("Build Harvester").Target("Harvester", 1);
+            
+            IPlan p = new BFSPlan();
+            p.Search(state, g0);
+
+            Assert.That(p.GetPath().Peek().Name, Is.EqualTo(actionName));
+        }
+
+        [Test]
+        public void PerformActionLeadingToGoal()
+        {
+            string actionName = "Build Harvester";
+            var state = new State();
+            state.PlanningActions.Add(new PlanningAction(actionName).Produces("Harvester"));
+            state.PlanningActions.Add(new PlanningAction("Not leading to goal").Produces("retsevraH"));
+            var g0 = new Goal("Build Harvester").Target("Harvester", 1);
+
+            IPlan p = new BFSPlan();
+            p.Search(state, g0);
+
+            Assert.That(p.GetPath().Peek().Name, Is.EqualTo(actionName));
+        }
+
+        [Test]
+        public void DontPerformAnyActionWhenNoActionIsAvailable()
+        {
+            var state = new State();
+            state.AddItem("Cash", 99);
+            var action = new PlanningAction("Not affordable").Consumes("Cash", 100).Produces("Harvester");
+            state.PlanningActions.Add(action);
+
+            var g0 = new Goal("Build Harvester").Target("Harvester", 1);
+
+            IPlan p = new BFSPlan();
+            p.Search(state, g0);
+
+            Assert.That(p.GetPath().Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void LimitDepthFirstPlanSearch()
+        {
+            var state = new State();
+            state.AddItem("Cash", 0);
+            var action = new PlanningAction("Get Salary").Produces("Cash",100);
+            state.PlanningActions.Add(action);
+
+            var g0 = new Goal("Accumulate Cash").Target("Cash", 1000);
+
+            IPlan p = new BFSPlan().SetMaxSearchDepth(3);
+            p.Search(state, g0);
+
+            Assert.That(p.GetPath().Count, Is.EqualTo(3));
+        }
+    }
+}
--- GOAP/Planners/BFSPlan.cs
+++ GOAP/Planners/BFSPlan.cs
@@ -7,32 +7,51 @@ namespace GOAP.Planners
 {
     public class BFSPlan : IPlan
     {
+        private int _maxSearchDepth = 5;
+        
         private Queue<State> stateProcessQueue = new Queue<State>();
+
+        // TODO: Remove the Tuple. Use something with better naming
+        private List<Tuple<State, PlanningAction>> takenPath = new List<Tuple<State, PlanningAction>>();
+
+        public BFSPlan SetMaxSearchDepth(int maxsearchdepth)
+        {
+            _maxSearchDepth = maxsearchdepth;
+            return this;
+        }
 
         public void Search(State state, Goal goal)
         {
+            int searchDepth = 0;
+
             stateProcessQueue.Enqueue(state);
 
-            InnerSearch(state, goal);
-        }
-
-        private void InnerSearch(State state, Goal goal)
-        {
             // Evaluate exit criteria
+            while (searchDepth < _maxSearchDepth && stateProcessQueue.Count > 0)
+            {
+                searchDepth++;
 
-            // Put each state in a queue
-
-            // Recurse
+                var currentState = stateProcessQueue.Dequeue();
+                
+                // Put each action in a queue
+                foreach (var action in state.PlanningActions.Where(l => l.CanExecute(state)))
+                {
+                    var neighbourState = action.Migrate(currentState); // Note: Feels backwards. I want to type currentState.Execute(action);
+                    // TODO: Don't add already visited states...
+                    takenPath.Add(new Tuple<State, PlanningAction>(currentState, action));
+                    stateProcessQueue.Enqueue(neighbourState);
+                }
+            }
         }
 
         public Stack<PlanningAction> GetPath()
         {
-            throw new NotImplementedException();
+            return new Stack<PlanningAction>(takenPath.Select(tuple => tuple.Item2).Reverse());
         }
 
         public State GetFinalState()
         {
-            throw new NotImplementedException();
+            return takenPath.Last().Item1;
         }
     }
 }
